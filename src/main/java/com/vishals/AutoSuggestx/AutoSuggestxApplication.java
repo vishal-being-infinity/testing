a648939fdc9b3ad2aa04da3bd9f123ec88e36e7e
vishal-being-infinity <vishal@being-infinity.com>
2026-07-17 10:40:25 +0530

    initial commit

---
+++ src/main/java/com/vishals/AutoSuggestx/AutoSuggestxApplication.java
@@ -0,0 +1,13 @@
+package com.vishals.AutoSuggestx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSuggestxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSuggestxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishals/AutoSuggestx/AutoSuggestxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishals.AutoSuggestx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
